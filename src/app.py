1ab1338263438cca2b7cd8278baa15a50bfe5495
William0487 <100438963+William0487@users.noreply.github.com>
2024-12-14 02:54:37 +0000

    Archivo Paso1

--- src/app.py
+++ src/app.py
@@ -5,4 +5,20 @@ import time
 import sqlite3
 import matplotlib.pyplot as plt
 import seaborn as sns
+import re 
+import pandas as pd
 
+
+ #Recurso a descargar
+resource_url = "https://www.macrotrends.net/stocks/charts/TSLA/tesla/revenue"
+
+# Petición para descargar el fichero de Internet
+response = requests.get(resource_url,headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"})
+
+# Si la petición se ha ejecutado correctamente (código 200), entonces el fichero se ha podido descargar
+if response:
+    # Se almacena el archivo en el directorio actual para usarlo más tarde
+    with open("tesla.csv", "wb") as dataset:
+        dataset.write(response.content)
+
+    
